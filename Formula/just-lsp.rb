5733b135d0a639c7755e64aa684b53a26cbd144b
Liam <liam@scalzulli.com>
2025-11-12 11:52:57 -0500

    Modify checksum for `just-lsp` formula

--- Formula/just-lsp.rb
+++ Formula/just-lsp.rb
@@ -2,7 +2,7 @@ class JustLsp < Formula
   desc "Language server for just"
   homepage "https://github.com/terror/just-lsp"
   url "https://github.com/terror/just-lsp/archive/refs/tags/0.2.8.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "b8fca056013f034951cf92d0dae1ffde3a2b18e192d4d39ebd9af15fefae9f1f"
   license "CC0-1.0"
   head "https://github.com/terror/just-lsp.git", branch: "main"
 
