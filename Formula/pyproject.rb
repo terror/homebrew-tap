b0b02eba8359831c20c777c14e4ce0f4f28e0339
liam <liam@scalzulli.com>
2025-11-27 14:50:43 -0500

    Update `pyproject` to 0.1.2 (#12)

--- Formula/pyproject.rb
+++ Formula/pyproject.rb
@@ -1,8 +1,8 @@
 class Pyproject < Formula
   desc "Linter and language server for pyproject.toml files"
   homepage "https://github.com/terror/pyproject"
-  url "https://github.com/terror/pyproject/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "af09e6a73f083448f0d5463c68836522e1fd5c1ebb4e375ad5a38d007347425a"
+  url "https://github.com/terror/pyproject/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "a734f0e8535be4c914fc84056ea2a6627392ca7868ae89ac1ad82e56141fd3a3"
   license "CC0-1.0"
   head "https://github.com/terror/pyproject.git", branch: "master"
 
