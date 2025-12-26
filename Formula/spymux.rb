9998132bc1bf3ef164225f29a5c81b51bb439bd3
Liam <liam@scalzulli.com>
2025-12-26 18:18:46 -0500

    spymux 0.1.2

--- Formula/spymux.rb
+++ Formula/spymux.rb
@@ -1,8 +1,8 @@
 class Spymux < Formula
   desc "Centralized view for all of your tmux panes"
   homepage "https://github.com/terror/spymux"
-  url "https://github.com/terror/spymux/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "5ad693327cb9e5d9de2fa3439498de8df9490c95b3fd00e0098ecc2b992380ef"
+  url "https://github.com/terror/spymux/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "24aa9d21d1d9e76cfa1a53de7ffeae236066027bb2729cd41491592860ee12ed"
   license "CC0-1.0"
   head "https://github.com/terror/spymux.git", branch: "master"
 
