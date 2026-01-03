0c599859ae6d2e97eb9d638f85e727f1dda8f544
Liam <liam@scalzulli.com>
2026-01-02 23:07:03 -0500

    swab 0.1.2

--- Formula/swab.rb
+++ Formula/swab.rb
@@ -1,8 +1,8 @@
 class Swab < Formula
   desc "Configurable project cleaning tool"
   homepage "https://github.com/terror/swab"
-  url "https://github.com/terror/swab/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "899b5cb4deb6cbe93afd7b28adddbf5d8f193812b29dcfac002641a4038da3fc"
+  url "https://github.com/terror/swab/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "39dd080caa629749a870f526dd84ad35459f0dbe8af7c43fbaf3a6c124ad8dbe"
   license "CC0-1.0"
   head "https://github.com/terror/swab.git", branch: "master"
 
