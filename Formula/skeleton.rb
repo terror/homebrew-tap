891fcc2c5c244fda7b8eb374548bbee1c802576d
Liam <liam@scalzulli.com>
2026-01-10 22:51:16 -0500

    skeleton 0.2.4

--- Formula/skeleton.rb
+++ Formula/skeleton.rb
@@ -1,8 +1,8 @@
 class Skeleton < Formula
   desc "Project scaffolding utility"
   homepage "https://github.com/terror/skeleton"
-  url "https://github.com/terror/skeleton/archive/refs/tags/0.2.3.tar.gz"
-  sha256 "8ccf115cdbb29a4a24de6d4965dc7098ea8d64bc0837bac0104cb366cb464575"
+  url "https://github.com/terror/skeleton/archive/refs/tags/0.2.4.tar.gz"
+  sha256 "e2060d6d905b11f410d7687d47286101d17cbbed3d8b80ddcc839f9c18a96f38"
   license "CC0-1.0"
   head "https://github.com/terror/skeleton.git", branch: "master"
 
