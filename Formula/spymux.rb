a3f65eda32d2707e400f10b5fe7d1bfbe14c10ff
liam <liam@scalzulli.com>
2026-01-26 21:56:13 -0500

    Update `spymux` to 0.1.3 (#26)

--- Formula/spymux.rb
+++ Formula/spymux.rb
@@ -1,8 +1,8 @@
 class Spymux < Formula
   desc "Centralized view for all of your tmux panes"
   homepage "https://github.com/terror/spymux"
-  url "https://github.com/terror/spymux/archive/refs/tags/0.1.2.tar.gz"
-  sha256 "24aa9d21d1d9e76cfa1a53de7ffeae236066027bb2729cd41491592860ee12ed"
+  url "https://github.com/terror/spymux/archive/refs/tags/0.1.3.tar.gz"
+  sha256 "457a4523967285ba1d864394022a31734c4474b01ff3ec08e874a70fbc8915b9"
   license "CC0-1.0"
   head "https://github.com/terror/spymux.git", branch: "master"
 
