8197c872e86d4ed03b8283dcf3e20aa35cc4aeb2
Liam <liam@scalzulli.com>
2026-02-10 00:56:21 -0500

    wt 0.1.2

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -1,8 +1,8 @@
 class Wt < Formula
   desc "Simple git worktree manager"
   homepage "https://github.com/terror/wt"
-  url "https://github.com/terror/wt/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "79cbcadb645b8d5b54631164e7de76eb83ecd803b10ab38d023b53f4bdfae7e4"
+  url "https://github.com/terror/wt/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "0e1af54e9701dd7bd6199e4e061f05b831eba6a1dedbf460a3ef41f1ddff22e5"
   license "CC0-1.0"
   head "https://github.com/terror/wt.git", branch: "master"
 
