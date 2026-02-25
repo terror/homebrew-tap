8d144d516d3a444173a8a057e23b08a0f074630e
Albert Seligmann <albertsseligmann@gmail.com>
2026-02-25 16:14:24 +0100

    just-lsp 0.3.3

--- Formula/just-lsp.rb
+++ Formula/just-lsp.rb
@@ -1,8 +1,8 @@
 class JustLsp < Formula
   desc "Language server for just"
   homepage "https://github.com/terror/just-lsp"
-  url "https://github.com/terror/just-lsp/archive/refs/tags/0.3.2.tar.gz"
-  sha256 "43fc1f2e5af9644f2e1e9093d3740eb40902732e38fa433b649f19e6e89d2bce"
+  url "https://github.com/terror/just-lsp/archive/refs/tags/0.3.3.tar.gz"
+  sha256 "65168f166114346eaaad8765f1518702d2cfa4785cf0605d6d557be46653ccd5"
   license "CC0-1.0"
   head "https://github.com/terror/just-lsp.git", branch: "main"
 
