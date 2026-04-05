a9c7a1580b718e255074d481d35d70b3d321fafa
liam <liam@scalzulli.com>
2026-04-04 23:21:41 -0400

    Update `just-lsp` to 0.4.1 (#32)

--- Formula/just-lsp.rb
+++ Formula/just-lsp.rb
@@ -1,8 +1,8 @@
 class JustLsp < Formula
   desc "Language server for just"
   homepage "https://github.com/terror/just-lsp"
-  url "https://github.com/terror/just-lsp/archive/refs/tags/0.3.4.tar.gz"
-  sha256 "de72ffd3ab6f201bf8e2ecab3dd98a1b21d5a55902f6c9132a90bb07d91cb752"
+  url "https://github.com/terror/just-lsp/archive/refs/tags/0.4.1.tar.gz"
+  sha256 "68b4b4d9e4cf9655b552d33431080c3e78304726e8ce59263a41bb7e464205d4"
   license "CC0-1.0"
   head "https://github.com/terror/just-lsp.git", branch: "main"
 
