ce6e2beb25ff452e7512a152aa2aa864a27de2da
liam <liam@scalzulli.com>
2026-04-10 22:50:15 -0400

    Update `axil` to 0.2.2 (#34)

--- Formula/axil.rb
+++ Formula/axil.rb
@@ -1,8 +1,8 @@
 class Axil < Formula
   desc "TUI for tree-sitter"
   homepage "https://github.com/terror/axil"
-  url "https://github.com/terror/axil/archive/refs/tags/0.2.1.tar.gz"
-  sha256 "1b3e95d85884d731c424aa9466af7343a35f1d4607e3b3353b08803c8d08b58e"
+  url "https://github.com/terror/axil/archive/refs/tags/0.2.2.tar.gz"
+  sha256 "3ec0c8d595afba97236cab9f6e7316dd9f47a437b821769ea37b46384a5c4b69"
   license "CC0-1.0"
   head "https://github.com/terror/axil.git", branch: "master"
 
