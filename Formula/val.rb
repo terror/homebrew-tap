d4ebf6255f1a0c6651ac7fb1306b1d6bc4bc10b3
Liam <liam@scalzulli.com>
2026-07-17 00:12:55 -0400

    val 0.4.0

--- Formula/val.rb
+++ Formula/val.rb
@@ -1,8 +1,8 @@
 class Val < Formula
   desc "Arbitrary precision calculator language"
   homepage "https://github.com/terror/val"
-  url "https://github.com/terror/val/archive/refs/tags/0.3.6.tar.gz"
-  sha256 "c3a19b91a83829709af2bf773c034361beb01b1cc7f5fa392a44f94579647ff7"
+  url "https://github.com/terror/val/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "38df3498c3103b3236999dc6fbc5a24295b5abd927950d074ffa7640a652a816"
   license "CC0-1.0"
   head "https://github.com/terror/val.git", branch: "master"
 
