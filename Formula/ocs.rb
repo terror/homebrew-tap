08cce778c23a6f7da2c7d68404522b35a8a5fe0e
liam <liam@scalzulli.com>
2026-07-27 13:44:11 -0400

    Update `ocs` to 0.1.2 (#49)

--- Formula/ocs.rb
+++ Formula/ocs.rb
@@ -1,8 +1,8 @@
 class Ocs < Formula
   desc "Session picker for opencode"
   homepage "https://github.com/terror/ocs"
-  url "https://github.com/terror/ocs/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "699f1ec485392c11e0cd0fb2d5a0fce445abe4679fcde70147a041175178e7f0"
+  url "https://github.com/terror/ocs/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "64b59e87f4825591510885da5f3c4f5d592032a0b5cd18b52ab57cd66cd007bc"
   license "CC0-1.0"
   head "https://github.com/terror/ocs.git", branch: "master"
 
